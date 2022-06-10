6a01b946227c8effb339a7a6148378a0eb1141a3
Tiago Seabra <4655377-tlgs@users.noreply.gitlab.com>
2022-06-10 16:32:51 +0100

    feat: init

---
+++ README.md
@@ -0,0 +1,11 @@
+# c02-link
+
+Hybrid ETH smart contract used for the [c02-labs](https://www.c02-labs.com/) platform.
+
+⚠️ Currently on development / prototyping stage.
+
+## Dev resources
+
+- [ChainlinkClient API Reference](https://docs.chain.link/docs/chainlink-framework/)
+- [Testnet Nodes and Jobs](https://docs.chain.link/docs/any-api-testnet-nodes/)
+- [Chainlink contracts v0.8 source](https://github.com/smartcontractkit/chainlink/tree/develop/contracts/src/v0.8)
---
+++ main.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import '@chainlink/contracts/src/v0.8/ChainlinkClient.sol';
+import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol';
+
+
+/**
+ * @notice A Chainlink client that fetches and hashes ESG data from
+ * the c02 labs platform.
+ * @dev Testnet implementation.
+ */
+contract ESGConsumer is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    bytes32 private jobId;
+    uint256 private fee;
+    bytes32 public hashedESGData;
+
+    event ObtainedData(bytes32 indexed requestId, bytes rawData);
+
+    /**
+     * @dev NETWORK: RINKEBY
+     */
+    constructor() ConfirmedOwner(msg.sender) {
+        setChainlinkToken(0x01BE23585060835E02B77ef475b0Cc51aA1e0709);
+        setChainlinkOracle(0xf3FBB7f3391F62C8fe53f89B41dFC8159EE9653f);
+        jobId = "7da2702f37fd48e5b1b9a5715e3509b6";
+        fee = (1 * LINK_DIVISIBILITY) / 10; // 0.1 LINK
+    }
+
+    /**
+     * @notice Requests ESG data from c02 endpoint
+     */
+    function requestESGData() public returns (bytes32 requestId) {
+        Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+
+        req.add("get", "https://c02-systems.ga/api/v1/show");
+        req.add("path", "0,content,form,overview,project_name");
+
+        return sendChainlinkRequest(req, fee);
+    }
+
+    /**
+     * @notice Callback function to return ESG data to this contract.
+     * @param _requestId bytes32
+     * @param _data bytes The data returned by the oracle.
+     */
+    function fulfill(bytes32 _requestId, bytes memory _data) public recordChainlinkFulfillment(_requestId) {
+        emit ObtainedData(_requestId, _data);
+        
+        hashedESGData = sha256(_data);
+    }
+
+    /**
+     * @notice Allow withdraw of Link tokens from the contract.
+     */
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+
+        require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
+    }
+}
