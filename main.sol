ab0dc40b23aeaffbcb93d0ba315832835f1fe0e7
Tiago Seabra <4655377-tlgs@users.noreply.gitlab.com>
2022-11-04 15:52:35 +0000

    Use new endpoint
    - instead of storing a single hash, we now store up to 256 entries
      using a ring buffer
    - endpoint provides the hash so there's no need to do any hashing
      ourselves
    - use Goerli network

--- main.sol
+++ main.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.12;
 
 import '@chainlink/contracts/src/v0.8/ChainlinkClient.sol';
 import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol';
 
-
 /**
- * @notice A Chainlink client that fetches and hashes ESG data from
+ * @notice A Chainlink client that fetches and ESG report data from
  * the c02 labs platform.
  * @dev Testnet implementation.
  */
@@ -15,9 +14,12 @@ contract ESGConsumerDemo is ChainlinkClient, ConfirmedOwner {
 
     bytes32 private jobId;
     uint256 private fee;
-    bytes32 public hashedESGData;
 
-    event ObtainedData(bytes32 indexed requestId, bytes rawData);
+    // ring buffer as storage
+    uint8 private idx;
+    string[256] public reportHashes;
+
+    event ObtainedData(bytes32 indexed requestId, string rawData);
 
     /**
      * @dev NETWORK: GOERLI
@@ -27,34 +29,36 @@ contract ESGConsumerDemo is ChainlinkClient, ConfirmedOwner {
         setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
         setChainlinkOracle(0xCC79157eb46F5624204f47AB42b3906cAA40eaB7);
 
-        // jobId with the Http -> JsonParse -> Ethabiencode tasks
-        jobId = "7da2702f37fd48e5b1b9a5715e3509b6";
+        // Testnet oracle job
+        // GET>string: Http -> JsonParse -> Ethabiencode tasks
+        jobId = "7d80a6386ef543a3abb52817f6707e3b";
 
         // requests on testnets cost 0.1 LINK
         fee = (1 * LINK_DIVISIBILITY) / 10;
     }
 
     /**
-     * @notice Requests ESG data from c02 platform endpoint.
+     * @notice Request report data for a specific client ID from c02 platform endpoint.
      */
-    function requestESGData() public returns (bytes32 requestId) {
+    function requestReportHash(string memory id) public returns (bytes32 requestId) {
         Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
 
-        req.add("get", "https://c02-systems.ga/api/v1/show");
-        req.add("path", "0,content,form,overview,project_name");
+        req.add("get", string.concat("https://tgdsyjp7ib.execute-api.us-east-1.amazonaws.com/miguel/report/", id, "/hash"));
+        req.add("path", "reportHash");
 
         return sendChainlinkRequest(req, fee);
     }
 
     /**
-     * @notice Callback function to return ESG data to this contract.
+     * @notice Callback function to return ESG report data to this contract.
      * @param _requestId bytes32
      * @param _data bytes The data returned by the oracle.
      */
-    function fulfill(bytes32 _requestId, bytes memory _data) public recordChainlinkFulfillment(_requestId) {
+    function fulfill(bytes32 _requestId, string memory _data) public recordChainlinkFulfillment(_requestId) {
         emit ObtainedData(_requestId, _data);
-        
-        hashedESGData = sha256(_data);
+
+        reportHashes[idx] = _data;
+        unchecked { idx += 1; }  // overflow me, baby
     }
 
     /**
@@ -62,7 +66,6 @@ contract ESGConsumerDemo is ChainlinkClient, ConfirmedOwner {
      */
     function withdrawLink() public onlyOwner {
         LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
-
         require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
     }
 }
